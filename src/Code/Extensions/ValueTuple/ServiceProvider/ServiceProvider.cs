a3bc61b8ee682b02d14a908720b13f08a724dac9
Shayne van Asperen <shaynevanasperen@gmail.com>
2019-09-20 00:24:05 +0100

    Add Code.Extensions.ValueTuple and Code.Extensions.ValueTuple.ServiceProvider
    (cherry picked from commit c135afd480d7b3cce6fb8a02e7e7c9892d86fe6e)

---
+++ src/Code/Extensions/ValueTuple/ServiceProvider/ServiceProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Code.Extensions.ValueTuple.ServiceProvider
+{
+	partial class ValueTupleServiceProvider : IServiceProvider
+	{
+		readonly IServiceProvider _serviceProvider;
+
+		public ValueTupleServiceProvider(IServiceProvider serviceProvider) => _serviceProvider = serviceProvider;
+
+		public object GetService(System.Type serviceType) =>
+			serviceType.IsValueTupleType()
+				? Activator.CreateInstance(serviceType, serviceType
+					.GetValueTupleItemTypes()
+					.Select(GetService)
+					.ToArray())
+				: _serviceProvider.GetService(serviceType);
+	}
+}
---
+++ src/Code/Extensions/ValueTuple/ValueTuple.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Code.Extensions.ValueTuple
+{
+	static partial class ValueTupleExtension
+	{
+		private static readonly ConcurrentDictionary<System.Type, Lazy<FieldInfo[]>> TuplesCache = new ConcurrentDictionary<System.Type, Lazy<FieldInfo[]>>();
+
+		private static readonly HashSet<System.Type> ValueTupleTypes = new HashSet<System.Type>(new System.Type[]
+		{
+			typeof(ValueTuple<>),
+			typeof(ValueTuple<,>),
+			typeof(ValueTuple<,,>),
+			typeof(ValueTuple<,,,>),
+			typeof(ValueTuple<,,,,>),
+			typeof(ValueTuple<,,,,,>),
+			typeof(ValueTuple<,,,,,,>),
+			typeof(ValueTuple<,,,,,,,>)
+		});
+
+		internal static bool IsValueTuple(this object obj) => IsValueTupleType(obj.GetType());
+
+		internal static bool IsValueTupleType(this System.Type type) =>
+			TuplesCache.GetOrAdd(type, x => x.IsValueTupleTypeInternal()
+				? new Lazy<FieldInfo[]>(() => GetItemFields(x))
+				: null) != null;
+
+		internal static object[] GetValueTupleItemObjects(this object tuple) =>
+			GetValueTupleItemFields(tuple.GetType()).Select(f => f.GetValue(tuple)).ToArray();
+
+		internal static System.Type[] GetValueTupleItemTypes(this System.Type type) =>
+			GetValueTupleItemFields(type).Select(f => f.FieldType).ToArray();
+
+		internal static FieldInfo[] GetValueTupleItemFields(this System.Type type) =>
+			TuplesCache.GetOrAdd(type, x => x.IsValueTupleTypeInternal()
+				? new Lazy<FieldInfo[]>(() => GetItemFields(x))
+				: null)?.Value ?? Array.Empty<FieldInfo>();
+
+		private static bool IsValueTupleTypeInternal(this System.Type type) =>
+			type.GetTypeInfo().IsGenericType && ValueTupleTypes.Contains(type.GetGenericTypeDefinition());
+
+		private static FieldInfo[] GetItemFields(System.Type key)
+		{
+			var items = new List<FieldInfo>();
+
+			FieldInfo field;
+			var nth = 1;
+			while ((field = key.GetRuntimeField($"Item{nth}")) != null)
+			{
+				nth++;
+				items.Add(field);
+			}
+
+			return items.ToArray();
+		}
+	}
+}
