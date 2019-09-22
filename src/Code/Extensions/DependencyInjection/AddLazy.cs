457f62dab206c04e2aebf67fa45b4a78b719c615
Shayne van Asperen <shaynevanasperen@gmail.com>
2019-09-23 00:26:11 +0100

    Add Code.Extensions.DependencyInjection.AddLazy

---
+++ src/Code/Extensions/DependencyInjection/AddLazy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Code.Extensions.DependencyInjection
+{
+	static partial class ServiceCollectionExtension
+	{
+		internal static IServiceCollection AddLazy(this IServiceCollection services) => services.AddTransient(typeof(Lazy<>), typeof(Lazier<>));
+	}
+
+	class Lazier<T> : Lazy<T> where T : class
+	{
+		public Lazier(IServiceProvider provider) : base(provider.GetRequiredService<T>) { }
+	}
+}
